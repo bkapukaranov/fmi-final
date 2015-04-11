8f121566d85fd0d999fa8f702500450c481d72f0
Borislav Kapukaranov <b.kapukaranov@gmail.com>
2015-04-11 18:26:57 +0300

    Adds high-level lexicon builder tool

---
+++ final_diploma/lexicon_builder.py
@@ -0,0 +1,9 @@
+__author__ = 'Borislav Kapukaranov'
+
+import review_util
+
+INPUT_BASE = '/Users/inspir3d/FMI/MasterDiploma/fmi-final/data/raw_reviews'
+INPUT = ['cinexio_movie_reviews_filtered.tsv', 'cinexio_movie_reviews_03-16.tsv']
+
+if __name__ == '__main__':
+    review_util.build_lexicon(INPUT_BASE, INPUT)
